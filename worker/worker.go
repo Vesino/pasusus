e0b5ec09a7261bd643fd42820f0c7864cbdc161b
Luis Vargas <luisvargasr10@gmail.com>
2024-01-25 15:56:36 -0600

    Worker methods skeletons

--- worker/worker.go
+++ worker/worker.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"fmt"
+
 	"github.com/Vesino/pasusus/task"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -11,4 +13,20 @@ type Worker struct {
 	Queue     queue.Queue
 	Db        map[uuid.UUID]*task.Task
 	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
 }
